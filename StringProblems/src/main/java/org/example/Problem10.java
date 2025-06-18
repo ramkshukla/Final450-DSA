d045ddbbf88198cbff33178a90e000e97a0187ee
ramkshukla <ramkumarshukla00000@gmail.com>
2025-06-18 16:28:00 +0530

    added problem10

---
+++ StringProblems/src/main/java/org/example/Problem10.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        String str = "abc";
+        System.out.println("All Subsequences of \"" + str + "\":");
+        printSubsequences(str, "");
+    }
+    public static void printSubsequences(String input, String output){
+        if(input.isEmpty()){
+            System.out.println(output);
+            return;
+        }
+        printSubsequences(input.substring(1), output + input.charAt(0));
+        printSubsequences(input.substring(1), output);
+    }
+}
