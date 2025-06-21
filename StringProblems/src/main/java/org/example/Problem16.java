c70979a9fd1a050f62ae52cd0865f55252817aec
ramkshukla <ramkumarshukla00000@gmail.com>
2025-06-21 22:01:35 +0530

    added problem 16 & 17

--- StringProblems/src/main/java/org/example/Problem16.java
+++ StringProblems/src/main/java/org/example/Problem16.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Problem16 {
@@ -7,22 +9,46 @@ public class Problem16 {
         String s = "[{()}]";
         System.out.println("String is Valid : " + isValid(s));
     }
-    public static boolean isValid(String str){
+    public static boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
         Stack<Character> stack = new Stack<>();
-        for(char ch : str.toCharArray()){
-            if(ch == '(' || ch == '{' || ch == '['){
-                stack.push(ch);
-            }
-            else {
-                if(stack.isEmpty()) return false;
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                        (ch == '}' && top != '{') ||
-                        (ch == ']' && top != '[')) {
+
+        for (char ch : s.toCharArray()) {
+            if (map.containsKey(ch)) {
+                char top = stack.isEmpty() ? '#' : stack.pop(); // '#' acts as dummy if stack is empty
+                if (top != map.get(ch)) {
                     return false;
                 }
+            } else {
+                stack.push(ch); // opening brackets
             }
         }
-        return stack.isEmpty();
+
+        return stack.isEmpty(); // all brackets matched
     }
+
+//
+//    public static boolean isValid(String str){
+//        Stack<Character> stack = new Stack<>();
+//        for(char ch : str.toCharArray()){
+//            if(ch == '(' || ch == '{' || ch == '['){
+//                stack.push(ch);
+//            }
+//            else {
+//                if(stack.isEmpty()) return false;
+//                char top = stack.pop();
+//                if ((ch == ')' && top != '(') ||
+//                        (ch == '}' && top != '{') ||
+//                        (ch == ']' && top != '[')) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+
 }
--- StringProblems/src/main/java/org/example/Problem17.java
+++ StringProblems/src/main/java/org/example/Problem17.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// Word Break Problem
 public class Problem17 {
+    public static void main(String[] args) {
+        String s = "applepenapple";
+        List<String> wordDict = Arrays.asList("apple", "pen");
+        boolean result = wordBreak(s, wordDict);
+        System.out.println("Can be segmented? " + result);
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict){
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+
+        for(int i=1; i <= n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && wordSet.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
 }
